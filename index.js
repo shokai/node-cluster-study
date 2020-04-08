e921625c94ade7dd03f396c4521124bac1c9e365
Sho Hashimoto <hashimoto@shokai.org>
2020-04-09 00:14:08 +0900

    4 worker起動

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+const cluster = require('cluster')
 const express = require('express')
 const { Server } = require('http')
 const morgan = require('morgan')
 
-const app = express()
-const server = Server(app)
+if (cluster.isMaster){
+  console.log(`start master(${process.pid})`)
+  for (let i = 0; i < 4; i++) {
+    cluster.fork()
+  }
 
-app.use(morgan('dev'))
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  const app = express()
+  const server = Server(app)
 
-app.get('/', (req, res) => {
-  res.send('hello')
-})
+  app.use(morgan('dev'))
 
-const port = process.env.PORT || 3000
-server.listen(port)
-console.log(`start server: port ${port}`)
+  app.get('/', (req, res) => {
+    res.send(`hello from worker(${process.pid})`)
+  })
+
+  const port = process.env.PORT || 3000
+  server.listen(port)
+  console.log(`start worker(${process.pid}): port ${port}`)
+}
