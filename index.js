c194e77f2c901ad9ae057e97fba5b42b3c28601d
Sho Hashimoto <hashimoto@shokai.org>
2020-04-09 00:20:16 +0900

    workerが死んだらforkして補充する

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ if (cluster.isMaster){
 
   cluster.on('exit', (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`)
+    cluster.fork()
   })
 } else {
   const app = express()
